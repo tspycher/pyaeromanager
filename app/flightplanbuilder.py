47152e642360c9b09cbf96fddb7bcf0003d6d43e
Thomas Spycher <me@tspycher.com>
2014-10-30 22:21:03 +0100

    worked on the pdf

--- app/flightplanbuilder.py
+++ app/flightplanbuilder.py
@@ -1,9 +1,9 @@
 __author__ = 'tspycher'
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4, A6
-from reportlab.platypus import Image
+from reportlab.platypus import Image, PageBreak, Spacer
 from reportlab.platypus.paragraph import Paragraph
-from reportlab.lib.styles import ParagraphStyle as PS
+#from reportlab.lib.styles import ParagraphStyle as PS
 
 
 from pyPdf import PdfFileReader, PdfFileWriter
@@ -28,10 +28,30 @@ class FlightplanBuilder(object):
         self.__fligthplan = flightplan
 
     def _takeoff(self):
-        filename = "/tmp/takeoff.pdf"
-        c = canvas.Canvas(filename, pagesize=A4)
-        c.drawString(100,100,"Hello World")
-        c.save()
+        filename = os.path.join(tempfile.gettempdir(), "%s.pdf" % 'takeoff')
+        elements = []
+        doc = PdfTemplate(filename=filename)
+        PS = doc.getStyleSheet()
+
+        doc.loadFrames()
+        elements.append(Paragraph("Takeoff Data", PS['h1']))
+        elements.append(Spacer(width=1, height=5))
+
+        elements.append(Paragraph("<b>QNH:</b> %s ft" % self.__fligthplan.performance_takeoff.qnh, PS['Normal']))
+        elements.append(Paragraph("<b>OAT:</b> %s C" % self.__fligthplan.performance_takeoff.oat, PS['Normal']))
+        elements.append(Paragraph("<b>PA:</b> %s ft" % self.__fligthplan.performance_takeoff.get_pa(), PS['Normal']))
+        elements.append(Paragraph("<b>DA:</b> %s ft" % self.__fligthplan.performance_takeoff.get_da(), PS['Normal']))
+        elements.append(Spacer(width=1, height=5))
+
+        elements.append(Paragraph("<b>Runway No:</b> %s" % self.__fligthplan.performance_takeoff.rwy_no, PS['Normal']))
+        elements.append(Paragraph("<b>Runway Ln.:</b> %s m" % self.__fligthplan.performance_takeoff.rwy_lenght, PS['Normal']))
+        elements.append(Paragraph("<b>Runway Type:</b> %s" % self.__fligthplan.performance_takeoff.rwy_type, PS['Normal']))
+        elements.append(Paragraph("<b>AD Code:</b> %s" % self.__fligthplan.performance_takeoff.aerodrome.code, PS['Normal']))
+        elements.append(Paragraph("<b>AD Name:</b> %s" % self.__fligthplan.performance_takeoff.aerodrome.name, PS['Normal']))
+        elements.append(Paragraph("<b>AD msl:</b> %s ft" % self.__fligthplan.performance_takeoff.aerodrome.msl, PS['Normal']))
+        elements.append(Spacer(width=1, height=5))
+
+        doc.build(elements)
         return filename
 
     def _airplane(self):
@@ -39,21 +59,20 @@ class FlightplanBuilder(object):
         airplane = self.__fligthplan.airplane
 
         elements = []
-        doc = PdfTemplate(filename=filename, pagesize=A6)
-        elements.append(Paragraph("This is text", PS('body')))
+        doc = PdfTemplate(filename=filename)
+        PS = doc.getStyleSheet()
 
-        #c = canvas.Canvas(filename, pagesize=A6)
-        # General Airplane information
-        #doc.drawString(10,10,"%s - %s" % (airplane.manufacturer, airplane.name))
+        elements.append(Paragraph("%s %s" % (airplane.manufacturer, airplane.name), PS['Normal']))
+
         # Drawing Charts
         for chart in self.__fligthplan.airplane.charts:
-            #doc.drawString(20, 10, chart.name)
+            elements.append(PageBreak())
+            elements.append(Paragraph(chart.name, PS['title']))
             elements.append(doc.image(chart.file.read()))
-            elements.append(Paragraph(chart.name, PS('body')))
+            elements.append(Paragraph(chart.description, PS['Normal']))
 
 
         doc.build(elements)
-        #doc.build(elements)
         return filename
 
     def buildPdf(self):
@@ -64,9 +83,6 @@ class FlightplanBuilder(object):
 
         self.addAllPages(output, PdfFileReader(file(to_pdffile, "rb")))
         self.addAllPages(output, PdfFileReader(file(ap_pdffile, "rb")))
-
-        #part11 = PdfFileReader(file(ap_pdffile, "rb"))
-        #output.addPage(part11.getPage(0))
 
         # Add AD Info Charts
         files = dict()
